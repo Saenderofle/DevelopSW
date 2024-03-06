a5e578bae8740b9503f0b7a8eaf308a8b4fc24ae
Saenderof <144450293+Saenderof@users.noreply.github.com>
2024-03-06 21:14:11 +0200

    "Ääîî ôövivaMechMat"

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ def print_hello_world():
     print("Hello, World")
 
 
+def vivaMechMat():
+    print("Viva Mech-mat faculty")
+
+
 def main():
     for _ in range(10):
         print_hello_world()
+    vivaMechMat()
 
 
 if __name__ == "__main__":
